a1f4eba838b00c126e863ed4b423006032a32896
Wei Huang <wei.x.huang@sonymobile.com>
2015-02-10 13:46:41 +0800

    Fix OOM issue of InCallUI
    CallButtonPresenter didn't remove
    itself from InCallPresenter(CanAddCallListener).
    
    Bug: 32266071
    Change-Id: I2ad979d9eebeb110b03e5a1c103d7a4f98e317c0
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- InCallUI/src/com/android/incallui/CallCardFragment.java
+++ InCallUI/src/com/android/incallui/CallCardFragment.java
@@ -1477,6 +1477,9 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
                         setViewStatePostAnimation(listener);
                         mIsAnimating = false;
                         InCallPresenter.getInstance().onShrinkAnimationComplete();
+                        if (animator != null) {
+                            animator.removeListener(this);
+                        }
                     }
                 });
                 animator.start();
