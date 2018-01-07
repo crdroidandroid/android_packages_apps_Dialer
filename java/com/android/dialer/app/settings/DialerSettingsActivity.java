52877d395e7d714cc555e3965f2d62c8687140c2
maxwen <max.weninger@gmail.com>
2018-01-07 16:24:08 +0100

    Dialer: Add option to disable proximity sensor
    Provide a switch for end user to disable proximity sensor
    while calling/incall.
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- java/com/android/dialer/app/settings/DialerSettingsActivity.java
+++ java/com/android/dialer/app/settings/DialerSettingsActivity.java
@@ -16,10 +16,13 @@
  */
 package com.android.dialer.app.settings;
 
+import static android.hardware.Sensor.TYPE_PROXIMITY;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserManager;
@@ -135,6 +138,14 @@ public class DialerSettingsActivity extends AppCompatActivity implements
       soundSettings.setViewId(R.id.settings_header_sounds_and_vibration);
       soundSettings.setIconSpaceReserved(false);
       getPreferenceScreen().addPreference(soundSettings);
+
+      if (showSensorOptions()) {
+        Preference sensorSettings = new Preference(getContext());
+        sensorSettings.setTitle(R.string.sensor_settings_title);
+        sensorSettings.setFragment(SensorSettingsFragment.class.getName());
+        sensorSettings.setIconSpaceReserved(false);
+        getPreferenceScreen().addPreference(sensorSettings);
+      }
 
       Preference quickResponseSettings = new Preference(getContext());
       Intent quickResponseSettingsIntent =
@@ -303,6 +314,11 @@ public class DialerSettingsActivity extends AppCompatActivity implements
      */
     private boolean isPrimaryUser() {
       return requireContext().getSystemService(UserManager.class).isSystemUser();
+    }
+
+    private boolean showSensorOptions() {
+      SensorManager sm = (SensorManager) requireContext().getSystemService(Context.SENSOR_SERVICE);
+      return sm.getDefaultSensor(TYPE_PROXIMITY) != null;
     }
   }
 }
