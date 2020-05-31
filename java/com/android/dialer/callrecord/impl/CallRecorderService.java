f69acdd385a74a5635cc54dd237935854c55ae94
Pranav Vashi <neobuddy89@gmail.com>
2020-06-01 00:13:13 +0530

    Dialer: Make recording names sortable
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- java/com/android/dialer/callrecord/impl/CallRecorderService.java
+++ java/com/android/dialer/callrecord/impl/CallRecorderService.java
@@ -55,7 +55,7 @@ public class CallRecorderService extends Service {
   private CallRecording mCurrentRecording = null;
   private MediaScannerConnectionClient mClient;
 
-  private SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyMMdd_HHmmssSSS");
+  private SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd-HHmmss");
 
   private final ICallRecorderService.Stub mBinder = new ICallRecorderService.Stub() {
     @Override
@@ -253,7 +253,7 @@ public class CallRecorderService extends Service {
 
     int formatChoice = getAudioFormatChoice();
     String extension = formatChoice == 0 ? ".amr" : ".m4a";
-    return number + "_" + timestamp + extension;
+    return "CallRecord_" + timestamp + "_" + number + extension;
   }
 
   public static boolean isEnabled(Context context) {
