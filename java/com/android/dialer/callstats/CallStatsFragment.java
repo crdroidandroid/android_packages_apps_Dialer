beccc05190c0251dc4f2d185c701ea04e0065b4a
minarypenguin <alexfinhart@gmail.com>
2022-05-06 18:33:48 +0800

    Dialer: Rework empty illustrations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- java/com/android/dialer/callstats/CallStatsFragment.java
+++ java/com/android/dialer/callstats/CallStatsFragment.java
@@ -114,7 +114,7 @@ public class CallStatsFragment extends Fragment implements
     mLayoutManager = new LinearLayoutManager(getActivity());
     mRecyclerView.setLayoutManager(mLayoutManager);
     mEmptyListView = (EmptyContentView) view.findViewById(R.id.empty_list_view);
-    mEmptyListView.setImage(R.drawable.empty_call_log);
+    mEmptyListView.setImage(R.drawable.oneplus_empty_call_log_illustration);
     mEmptyListView.setActionClickedListener(this);
 
     mSumHeaderView = (TextView) view.findViewById(R.id.sum_header);
