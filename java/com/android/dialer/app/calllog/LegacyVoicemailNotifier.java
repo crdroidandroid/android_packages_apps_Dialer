ec2140562d880fe1ad4dcbff7fc857285947b0f9
Pranav Vashi <neobuddy89@gmail.com>
2025-02-28 02:03:34 +0530

    Dialer: Fix crash on voicemail notifier
    Log:
    
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: FATAL EXCEPTION: main
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: Process: com.android.dialer, PID: 2532
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: java.lang.RuntimeException: Unable to start receiver com.android.dialer.app.voicemail.LegacyVoicemailNotificationReceiver: java.lang.IllegalArgumentException
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.app.ActivityThread.handleReceiver(ActivityThread.java:4862)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.app.ActivityThread.-$$Nest$mhandleReceiver(Unknown Source:0)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2476)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:109)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:232)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:317)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8787)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:594)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:871)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: Caused by: java.lang.IllegalArgumentException
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.dialer.common.Assert.checkArgument(PG:103)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.dialer.common.Assert.checkArgument(PG:89)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.dialer.theme.base.impl.AospThemeImpl.getColorPrimary(PG:143)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.dialer.app.calllog.LegacyVoicemailNotifier.createNotification(PG:143)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.dialer.app.calllog.LegacyVoicemailNotifier.showNotification(PG:76)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at com.android.dialer.app.voicemail.LegacyVoicemailNotificationReceiver.onReceive(PG:109)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	at android.app.ActivityThread.handleReceiver(ActivityThread.java:4853)
    02-25 15:17:02.011  2532  2532 E AndroidRuntime: 	... 9 more
    
    Likely missed in: https://github.com/crdroidandroid/android_packages_apps_Dialer/commit/507cdc7bbcefd13f39eea51e86fa763138237afe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- java/com/android/dialer/app/calllog/LegacyVoicemailNotifier.java
+++ java/com/android/dialer/app/calllog/LegacyVoicemailNotifier.java
@@ -140,7 +140,7 @@ public final class LegacyVoicemailNotifier {
     Notification.Builder builder =
         new Notification.Builder(context)
             .setSmallIcon(resId)
-            .setColor(ThemeComponent.get(context).theme().getColorPrimary())
+            .setColor(ThemeComponent.get(context).theme().getColorCallNotificationBackground())
             .setWhen(System.currentTimeMillis())
             .setContentTitle(notificationTitle)
             .setContentText(contentText)
